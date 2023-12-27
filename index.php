1272c7f3480a477a4af319c78566e517c032eaf9
utopicUnicorn <christianbarrios995@gmail.com>
2023-12-27 19:39:44 +0800

    wordpress: added single.php page

--- index.php
+++ index.php
@@ -1,27 +1,41 @@
-<?php 
-function myFirstFunction($name, $name2){
-    echo "<p>Hi Bitch. My name is $name. You Mother fucking $name2 hahaha</p>";
-}
-myFirstFunction("fcker", "Joe");
-?>
+<!-- <?php
+        // function myFirstFunction($name, $name2){
+        // echo "<p>Hi Bitch. My name is $name. You Mother fucking $name2 hahaha</p>";
+        // }
+        // myFirstFunction("fcker", "Joe");
+        ?>
 
 <h1>
-    <?php bloginfo('name');?>
+  <?php  // bloginfo('name');
+    ?>
 </h1>
 
 <p>
-    <?php bloginfo('description');?>
+     <?php
+        //bloginfo('description');
+        ?> 
 </p>
 
 <ul>
-<?php 
-    $myName = "Ian";
-    $names = array('Ian', "John", 'Doe');
-    echo $names[1];
-    $count = 1;
-    while($count < 100){
-        echo "<li>$count</li>";
-        $count++;
-    }
+<?php
+// $myName = "Ian";
+// $names = array('Ian', "John", 'Doe');
+// echo $names[1];
+// $count = 0;
+// while($count < count($names) ){
+//     echo "<li>Hi bitch! My name is $names[$count]</li>";
+//     $count++;
+// }
 ?>
-</ul>
+</ul> -->
+
+<?php
+while (have_posts()) {
+    the_post(); //The_post function will keep tract of which post we are currently working in
+?>
+    <h2><a href="<?php the_permalink()?>"><?php the_title() ?></a></h2>
+    <?php the_content() ?>
+    <hr>
+<?php
+}
+?>
---
+++ single.php
@@ -0,0 +1,2 @@
+asdasd
+<!-- Single php -->
