25550e7f1919b0c6965d297c1ced52be1363f843
utopicUnicorn <christianbarrios995@gmail.com>
2024-01-22 16:07:54 +0800

    fictional university: moved a function from functions.php

--- functions.php
+++ functions.php
@@ -25,5 +25,8 @@ function university_features(){
     add_theme_support('title-tag');
 }
 
-add_action('after_setup_theme', 'university_features')
+
+
+add_action('after_setup_theme', 'university_features');
+
 ?>
